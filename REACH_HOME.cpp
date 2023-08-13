cc8e611d26baed76c588b7e93e2c674c0f1ceda2
Pardarshee <pardarsheepriyal@gmail.com>
2023-08-13 15:12:05 +0530

    Difficulty level 395

---
+++ REACH_HOME.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	
+	int x, y;
+	while(t){
+	    cin >> x >> y;
+	    
+	    if((x*5) >= y){
+	        cout << "YES" << endl;
+	    }
+	    else{
+	        cout << "NO" << endl;
+	    }
+	    t--;
+	}
+	return 0;
+}
