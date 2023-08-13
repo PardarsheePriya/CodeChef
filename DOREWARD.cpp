91ed9593d9af36b64a3cdb4bab5d0ed629a3e106
Pardarshee <pardarsheepriyal@gmail.com>
2023-08-13 15:06:10 +0530

    395 difficulty level

---
+++ DOREWARD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	int x;
+	while(t){
+	    
+	    cin >> x;
+	    
+	    if(x <= 3){
+	        cout << "BRONZE" <<endl;
+	    }
+	    else if(x > 3 && x <= 6){
+	        cout << "SILVER" << endl;
+	    }
+	    else{
+	        cout << "GOLD" << endl;
+	    }
+	    t--;
+	}
+	return 0;
+}
