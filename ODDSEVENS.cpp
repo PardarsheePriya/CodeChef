36c190cd23c197a53d6602602d712ca643df2107
Pardarshee <pardarsheepriyal@gmail.com>
2023-08-26 09:51:30 +0530

    Difficulty Level 872

---
+++ ODDSEVENS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int a, b;
+	    cin >> a >> b;
+	    
+	    int sum = a+b;
+	    if(sum%2 == 0){
+	        cout << "Bob" << endl;
+	    }
+	    else{
+	        cout << "Alice" << endl;
+	    }
+	}
+	return 0;
+}
